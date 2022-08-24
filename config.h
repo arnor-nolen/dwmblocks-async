818f04401a6e7c3f81565baab83e1f348bbd419e
Arnor Nolen <andrey1gorbulin@gmail.com>
2022-08-24 14:41:58 +0300

    Added keyboard layout

--- config.h
+++ config.h
@@ -7,5 +7,6 @@ const Block blocks[] = {
 	BLOCK("free -t | awk 'NR == 2 {printf(\" %3.f%\"), $3/$2*100}'", 15, 0),
 	BLOCK("pulsemixer --get-volume | awk '{printf(\" %3d%\"), $1}'", 0, 1),
         BLOCK("~/sum_work.sh | awk '{printf(\" %4.1fh\"), $1}'", 15, 0),
-	BLOCK("date '+%a %F %I:%M %p]'", 5, 0),
+	BLOCK("date '+%a %F %I:%M %p'", 5, 0),
+        BLOCK("setxkbmap -print -verbose 10 | grep layout | awk '{printf(\"%s]\"), $2}' ", 0, 2),
 };
