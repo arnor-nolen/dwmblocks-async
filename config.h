0122bbdf3ed47cda252c6215fa54b0f081d896b6
Arnor Nolen <andrey1gorbulin@gmail.com>
2022-09-14 22:36:51 +0300

    Changed cpu icon

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 const Block blocks[] = {
         BLOCK("pacman -Qu | wc -l | awk '{printf(\"[ %4d\"), $1}'", 15, 0),
         BLOCK("~/sum_work.sh | awk '{printf(\"華 %4.1fh\"), $1}'", 15, 0),
-	BLOCK("awk -v a=\"$(awk '/cpu /{print $2+$4,$2+$4+$5}' /proc/stat; sleep 1)\" '/cpu /{split(a,b,\" \"); {printf(\"﬙ %3.f%\"), 100*($2+$4-b[1])/($2+$4+$5-b[2])}}'  /proc/stat", 15, 0),
+	BLOCK("awk -v a=\"$(awk '/cpu /{print $2+$4,$2+$4+$5}' /proc/stat; sleep 1)\" '/cpu /{split(a,b,\" \"); {printf(\" %3.f%\"), 100*($2+$4-b[1])/($2+$4+$5-b[2])}}'  /proc/stat", 15, 0),
 	BLOCK("free -t | awk 'NR == 2 {printf(\" %3.f%\"), $3/$2*100}'", 15, 0),
         BLOCK("(pulsemixer --get-mute; pulsemixer --get-volume) | awk 'NR==1{printf(\"%s\"), $1 ? \"ﱝ \": \"奔 \" };NR==2{printf(\"%3d%\"), $1}'", 0, 1),
 	BLOCK("date '+%a %F %I:%M %p'", 5, 0),
